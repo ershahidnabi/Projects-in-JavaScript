a40770dd0049f717fffe6f79213fb5f52043eb5f
ershahidnabi <ershahidnabi@gmail.com>
2024-05-08 20:37:47 +0530

    Simple Bg Color Changer

---
+++ bg-changer/bg-changer.js
@@ -0,0 +1,23 @@
+console.log("@ershahidnabi");
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener("click", function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === "gray") {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === "teal") {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === "orange") {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === "purple") {
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
